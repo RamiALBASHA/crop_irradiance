0143457028e80b6a076ba54cf078f301304e1992
ralbasha <rami.albasha@itk.fr>
2019-12-11 18:16:13 +0100

    wip tests

--- test/test_uniform_crops/test_sunlit_shaded_leaves.py
+++ test/test_uniform_crops/test_sunlit_shaded_leaves.py
@@ -13,3 +13,19 @@ def test_calc_direct_black_extinction_coefficient_is_maximum_at_sunrise_and_suns
 
     assert extinction_coef_at_sunrise > extinction_coef_at_midday
     assert extinction_coef_at_sunset > extinction_coef_at_midday
+
+
+def test_calc_direct_extinction_coefficient_is_zero_when_leaf_scattering_is_unity():
+    leaf_scattering_coefficient = 1.
+    solar_inclination = pi / 3.
+    assert sunlit_shaded_leaves.calc_direct_extinction_coefficient(solar_inclination, leaf_scattering_coefficient) == 0.
+
+
+def test_calc_direct_extinction_coefficient_returns_expected_value():
+    leaf_scattering_coefficient = 0.15
+    solar_inclination = pi / 2.
+    expected_value = 0.4609772228646444
+    actual_value = sunlit_shaded_leaves.calc_direct_extinction_coefficient(solar_inclination,
+                                                                           leaf_scattering_coefficient)
+    testing.assert_almost_equal(actual_value, expected_value, decimal=6)
+
