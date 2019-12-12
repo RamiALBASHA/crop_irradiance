cab381f4677e93b9c247910871ba490c4a0fcee9
ralbasha <rami.albasha@itk.fr>
2019-12-12 19:10:55 +0100

    wip testing sunlit shaded

--- test/test_uniform_crops/test_sunlit_shaded_leaves.py
+++ test/test_uniform_crops/test_sunlit_shaded_leaves.py
@@ -1,4 +1,4 @@
-from numpy import testing, linspace, pi
+from numpy import testing, linspace, pi, arange
 from crop_irradiance.uniform_crops import sunlit_shaded_leaves
 
 
@@ -29,3 +29,54 @@ def test_calc_direct_extinction_coefficient_returns_expected_value():
                                                                            leaf_scattering_coefficient)
     testing.assert_almost_equal(actual_value, expected_value, decimal=6)
 
+
+def test_calc_sky_sectors_weight_returns_expected_uoc_values():
+    assert sunlit_shaded_leaves.calc_sky_sectors_weight(3, 'uoc') == [0.25, 0.5, 0.25]
+
+
+def test_calc_sky_sectors_weight_returns_expected_soc_values():
+    actual_values = sunlit_shaded_leaves.calc_sky_sectors_weight(3, 'soc')
+    expected_values = [0.1785714285714285, 0.5140108873361879, 0.3074176840923834]
+    testing.assert_almost_equal(actual_values, expected_values, decimal=6)
+
+
+def test_calc_sky_sectors_weight_returns_empty_list_for_unknown_sky_type():
+    assert sunlit_shaded_leaves.calc_sky_sectors_weight(3, 'some unkonwn sky type') == []
+
+
+def test_calc_sky_sectors_weight_returns_weights_which_sums_up_to_1():
+    for sky_type in ('soc', 'uoc'):
+        actual_value = sum(sunlit_shaded_leaves.calc_sky_sectors_weight(3, sky_type))
+        expected_value = 1.0
+        testing.assert_almost_equal(actual_value, expected_value, decimal=6)
+
+
+def test_calc_diffuse_extinction_coefficient_reduces_as_leaf_area_index_increases():
+    leaf_scattering_coefficient = 0.15
+    diffuse_coeffcients = [
+        sunlit_shaded_leaves.calc_diffuse_extinction_coefficient(leaf_area_index, leaf_scattering_coefficient) for
+        leaf_area_index in arange(0, 5.1, 0.1)]
+    assert all([x > y for x, y in zip(diffuse_coeffcients, diffuse_coeffcients[1:])])
+
+
+def test_calc_diffuse_extinction_coefficient_returns_greater_extinction_coefficient_for_black_leaves():
+    leaf_area_index = 3.0
+    leaf_scattering_coefficient = 0.15
+    sky_sectors_number = 3
+    for sky_type in ('soc', 'uoc'):
+        extinction_coef, black_extinction_coef = sunlit_shaded_leaves.calc_diffuse_extinction_coefficient(
+            leaf_area_index, leaf_scattering_coefficient, sky_sectors_number, sky_type)
+    assert extinction_coef < black_extinction_coef
+
+
+def test_calc_diffuse_extinction_coefficient_returns_expected_values():
+    leaf_area_index = 3.0
+    leaf_scattering_coefficient = 0.15
+    sky_sectors_number = 3
+    expected_values = {'soc': (0.6390947959070368, 0.6872841045179627),
+                       'uoc': (0.675874154734713, 0.7250108165502616)}
+
+    for sky_type in ('soc', 'uoc'):
+        actual_values = sunlit_shaded_leaves.calc_diffuse_extinction_coefficient(
+            leaf_area_index, leaf_scattering_coefficient, sky_sectors_number, sky_type)
+    testing.assert_almost_equal(actual_values, expected_values[sky_type], decimal=6)
